e495180ea156bb0d00aee94ccb830e1557d726c7
Bidhan <bidhan.devkota@gmail.com>
2022-10-03 22:09:42 -0500

    yes

---
+++ app/src/main/java/com/example/flashy/AddCardActivity.kt
@@ -0,0 +1,42 @@
+package com.example.flashy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
+
+class AddCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+val question=findViewById<EditText>(R.id.QuestionField);
+        val answer =findViewById<EditText>(R.id.AnswerField);
+
+        findViewById<View>(R.id.myCancelBtn).setOnClickListener {
+            finish()
+        }
+        findViewById<View>(R.id.mySaveButton).setOnClickListener {
+            val quesString= question.text.toString();
+            val ansString=answer.text.toString();
+            val data = Intent() // create a new Intent, this is where we will put our data
+
+            data.putExtra(
+                "Question",
+                quesString
+            ) // puts one string into the Intent, with the key as 'string1'
+
+            data.putExtra(
+                "Answer",
+                ansString
+            ) // puts another string into the Intent, with the key as 'string2
+
+            setResult(RESULT_OK, data) // set result code and bundle data for response
+
+            finish()
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/flashy/MainActivity.kt
+++ app/src/main/java/com/example/flashy/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.flashy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +18,30 @@ class MainActivity : AppCompatActivity() {
             flashcardQuestion.visibility = View.INVISIBLE
             flashcardAnswer.visibility = View.VISIBLE
         }
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data: Intent? = result.data
+            if (data != null) { // Check that we have data returned
+                val Question = data.getStringExtra("Question") // 'string1' needs to match the key we used when we put the string in the Intent
+                val Answer = data.getStringExtra("Answer")
+                flashcardQuestion.text = Question
+                flashcardAnswer.text = Answer
+                // Log the value of the strings for easier debugging
+                Log.i("MainActivity", "Question: $Question")
+                Log.i("MainActivity", "Answer: $Answer")
+            } else {
+            Log.i("MainActivity", "Returned null data from AddCardActivity")
+        }
+        }
+        findViewById<View>(R.id.myBtn).setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+
+
+            resultLauncher.launch(intent)
+        }
+
+
+
+
+
     }
 }
